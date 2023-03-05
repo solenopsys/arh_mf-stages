35fba01d044374bab9d329a585b3486e749b2ef4
AlexStorm <info@alexstorm.ru>
2023-03-05 09:23:25 +0530

    fix names

--- src/app/remote-entry/entry.component.ts
+++ src/app/remote-entry/entry.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'hyperconverged-equpment-entry',
+  selector: 'sc-equpment-entry',
   template: `<div class="remote-entry">
     <h2>equpment's Remote Entry Component</h2>
   </div>`,
