76d9600f05a32e99e9ae3433ea40b6bbd7fad2ae
AlexStorm <info@alexstorm.ru>
2023-08-01 20:45:31 +0530

    stages conf from json + menu

--- src/stages/stages.component.ts
+++ src/stages/stages.component.ts
@@ -1,20 +1,65 @@
-import { Component } from '@angular/core';
-import {async, Observable} from "rxjs";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Observable} from "rxjs";
 import {HttpClient} from "@angular/common/http";
+import {ActivatedRoute} from "@angular/router";
+import {DataLoadRequest, HideComponent, MenuLoaderProvider, MenuLoaderService} from "@solenopsys/ui-templates";
+import {Store} from "@ngxs/store";
+import {GroupService} from "@solenopsys/ui-publications";
+import {MenuItemData} from "@solenopsys/ui-navigate";
+
+
+type Stage = {
+    name: string,
+    id:string
+}
+
+class StagesProvider implements MenuLoaderProvider {
+
+    constructor(public $stages: Observable<Stage[]>) {
+    }
+
+
+    load(dataKey: string): Promise<MenuItemData[]> {
+        return new Promise((resolve) => {
+            this.$stages.subscribe(stages => {
+                let source: MenuItemData[] = stages.map<MenuItemData>(stage => {
+                    return {name: stage.name, link: "/stages#"+stage.id}
+                });
+                resolve(source);
+            })
+        });
+    }
+
+}
 
 @Component({
-  selector: 'stages',
-  templateUrl: './stages.component.html',
-  styleUrls: ['./stages.component.css']
+    selector: 'stages',
+    templateUrl: './stages.component.html',
+    styleUrls: ['./stages.component.css']
 })
-export class StagesComponent {
-  statges$:Observable<any>
+export class StagesComponent implements OnInit,OnDestroy{
+    $stages: Observable<Stage[]>;
 
-  constructor(private http: HttpClient) {
-  }
-  ngOnInit(): void {
-    this.statges$= this.http.get("/modules/solenopsys/mf-stages/src/assets//stages.json")
-  }
+    constructor(private http: HttpClient,
+                private ar: ActivatedRoute,
+                private store: Store,
+                private groupService: GroupService,
+                private mls: MenuLoaderService) {
+    }
 
-  protected readonly async = async;
+    ngOnInit(): void {
+        this.$stages= this.http.get<Stage[]>(this.ar.snapshot.data['url']);
+
+        const dataKey = "test_data"; // todo remove hardcode
+
+        this.mls.addProvider("stages_provider", new StagesProvider(this.$stages));
+
+        this.mls.addMapping(dataKey, "stages_provider");
+ //       this.store.dispatch(new DataLoadRequest("left_menu", dataKey));
+    }
+
+    ngOnDestroy(): void {
+        this.store.dispatch(new HideComponent("left_menu"));
+    }
+
 }
